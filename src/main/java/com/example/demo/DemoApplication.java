f6399f28329fb9f6ac59df425e99289db1d87099
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2026-07-24 15:32:46 +0900

    수업

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
 import com.example.demo.i0708.AgilityHero;
+import com.example.demo.i0708.Hero;
 import com.example.demo.i0708.StrengthHero;
-import com.example.demo.studentmanagement.Person;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 
 @SpringBootApplication
 public class DemoApplication {
+
+    /*
     private static final Map<Integer, Player> PLAYERS = new HashMap<>(){
         {
             // Radiant 래디언트 진영
@@ -27,42 +30,60 @@ public class DemoApplication {
             put(9, new Player(9, "Iaron", Side.DIRE, 8, 2, 2, new AgilityHero("Phantom Lancer")));
             put(10, new Player(10, "Jaron", Side.DIRE, 1, 8, 7, new AgilityHero("Drow Ranger")));
         }
-    };
+    };*/
 
     public static void main(String[] args) {
+
+        // 중간 연산자
+        // 최종 연산자
+        List<Player> playerList = new ArrayList<>(Game.HISTORY.get(0).getPlayers().values());
+
+        System.out.println(" -- Stream.forEach -- ");
+        Stream<Player> playerStream = playerList.stream();
+//      Stream<String> intermediate = playerList.stream() // Stream<Player> = [Player, Player, Player]
+        Map<String, Hero> intermediate = playerList.stream()
+                /* Stream<Player> */ .peek(player -> System.out.println(" >> 첫번째 중간 연산자가 실행됩니다 << "))
+                /* Stream<Player> */ .filter(player -> player.getSide().equals(Side.RADIANT))
+                /* Stream<Player> */ .peek(player -> System.out.println(" >> 두번째 중간 연산자가 실행됩니다 << "))
+                /* Stream<Player> */ .filter(player -> player.getKill() >= 5)
+                /* Stream<Player> */ .peek(player -> System.out.printf(" >> 세번째 중간 연산자가 실행되기 전 : %s << \n", player))
+                /* Stream<Hero>   */ .map(Player::getPickedHero)
+//              /* Stream<String> */ .map(Hero::getName) // 실습 내 이거 삭제하고 Stream<Hero> 반환하게 바꾼 뒤 오류 수정하기
+//                                   .collect(Collectors.toList()); // 정렬 되어 나옴
+//                                   .collect(Collectors.toSet());  // 어떤 순서로 나오는지는 모르지만 섞여나옴
+                                    .collect(Collectors.toMap(
+                                        (hero) -> hero.getName(),
+                                        (hero) -> hero
+                                    ));
+
+        System.out.println(" -- 중간 연산자들을 적용 완료 / 이제 최종 연산자를 통해 실행 -- ");
+        System.out.println(intermediate);
+//      intermediate.forEach(System.out::println);
+//      intermediate.forEach((heroName) -> System.out.println(heroName));
+
+        /*
         List<Player> playerList = new ArrayList<>(PLAYERS.values());
-        final int[] a = {1};
-        final int[] b ={1};
 
         System.out.println("-- Stream.forEach --");
         Stream<Player> playerStream = playerList.stream();
 
-        Stream<Player> intermediate = playerList.stream()
-                .filter((plyaer) -> {
-                    System.out.println(">> 첫번째 중간 연산자 실행: " + a[0]);
-                    a[0]++;
-                    return plyaer.getSide().equals(Side.RADIANT);
-                })
-                .filter(player -> {
-                    System.out.println(">> 두번째 중간 연산자 실행: "+ b[0]);
-                    b[0]++;
-                    return player.getKill()>=5;
-                })
-                .filter(new Predicate<Player>() {
-                    @Override
-                    public boolean test(Player player) {
-                        System.out.println(">> 세번째 중간 연산자 실행");
-                        return player.getSide().equals(Side.RADIANT);
-                    }
-                });
+        Stream<String> intermediate = playerList.stream()
+                .peek(player -> System.out.println(">> 첫번째 중간 연산자 실행 <<"))
+                .filter(player -> player.getSide().equals(Side.RADIANT))
+                .peek(player -> System.out.println(">> 두번째 중간 연산자 실행 <<"))
+                .filter(player -> player.getKill()>=5)
+                .peek(player -> System.out.printf(">> 세번째 중간 연산자가 실행되기 전: %s <<\n", player))
+                .map(Player::getPickedHero)
+                .map(Hero::getName)
+                ;
 
         intermediate
-                .forEach((new Consumer<Player>() {
+                .forEach((new Consumer<String>() {
                     @Override
-                    public void accept(Player player) {
-                        System.out.println(player);
+                    public void accept(String heroName) {
+                        System.out.println(heroName);
                     }
-                }));
+                }));*/
 
 /*        System.out.println("-- 향상된 for --");
         for (Player player:playerList){
--- src/main/java/com/example/demo/i0710/HeroInformation.java
+++ src/main/java/com/example/demo/i0710/HeroInformation.java
@@ -1,5 +1,6 @@
 package com.example.demo.i0710;
 
+
 public interface HeroInformation {
     public abstract String getName();
 }
